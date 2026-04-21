50cec5c622c1cd583702c78136518952e4556b8a
Marco <marco@bon.so>
2026-04-21 16:33:14 +0200

    feat: add vim-rails

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -17,13 +17,38 @@ return {
     "h3pei/copy-file-path.nvim",
   },
 
-  -- Rails support
-  {
-    "tpope/vim-rails",
-  },
-
   -- Project-wide alternate file definitions
   {
     "tpope/vim-projectionist",
+  },
+
+  -- Rails support
+  {
+    "tpope/vim-rails",
+    dependencies = { "tpope/vim-projectionist" },
+    ft = { "ruby", "eruby", "haml", "slim" },
+    cmd = {
+      "Rails",
+      "Rgenerate",
+      "Rrunner",
+      "Econtroller",
+      "Emodel",
+      "Eview",
+      "Emigration",
+      "Eschema",
+      "A",
+      "AV",
+      "AS",
+      "AT",
+    },
+    init = function()
+      vim.g.rails_projections = {
+        ["app/services/*.rb"] = { command = "service" },
+        ["app/workers/*.rb"] = { command = "worker" },
+        ["app/jobs/*.rb"] = { command = "job" },
+        ["app/policies/*.rb"] = { command = "policy" },
+        ["app/serializers/*.rb"] = { command = "serializer" },
+      }
+    end,
   },
 }
